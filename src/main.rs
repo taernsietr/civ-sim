bad75a514f30d232ff23cbf06bd87f320aac6ef7
Thalles Rodrigues <ts.rodr@gmail.com>
2024-02-19 13:56:32 -0300

    clicks generate new world

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use nannou::prelude::*;
+use nannou::{prelude::*, state::mouse};
 use lazy_static::lazy_static;
 // use rayon::prelude::*;
 use crate::{
@@ -16,7 +16,6 @@ pub mod noise_sampler;
 
 lazy_static! {
     static ref ARGS: Args = Args::parse();
-    static ref WORLDS: Vec<World> = generate_worlds(&ARGS);
 }
 
 fn main() {
@@ -39,23 +38,31 @@ fn main() {
     //    save_image(world, VisualizationMode::Biome, args.file.clone(), args.debug);
     //});
 
-    nannou::app(model).update(update).run();
+    nannou::app(model).run();
 }
 
 struct Model {
     _window: window::Id,
+    worlds: Vec<World>,
     texture: Texture,
 }
 
 fn model(app: &App) -> Model {
-    let _window = app.new_window().view(view).build().unwrap();
-    let image = generate_image(&WORLDS[0], VisualizationMode::Biome);
+    let _window = app.new_window().mouse_pressed(mouse_pressed).view(view).build().unwrap();
+    let worlds: Vec<World> = generate_worlds(&ARGS);
+    let image = generate_image(&worlds[0], VisualizationMode::Biome);
     let converted = nannou::image::DynamicImage::ImageRgb8(image);
     let texture = Texture::from_image(app, &converted);
-    Model { _window, texture }
+    Model { _window, worlds, texture }
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) { }
+fn mouse_pressed(app: &App, model: &mut Model, _key: MouseButton) {
+    model.worlds = generate_worlds(&ARGS);
+    let image = generate_image(&model.worlds[0], VisualizationMode::Biome);
+    let converted = nannou::image::DynamicImage::ImageRgb8(image);
+    model.texture = Texture::from_image(app, &converted);
+}
+
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
